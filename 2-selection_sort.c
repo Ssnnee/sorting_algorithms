70e32627ecdd23db6d5e67990f8274a6d97bd170
Ssnnee <nandisamuelsne@gmail.com>
2023-09-25 16:26:36 -0700

    Selection sort algorithm with C

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - Swap the values of two integers.
+ *
+ * @xp: Pointer to the first integer to be swapped.
+ * @ap: Pointer to the second integer to be swapped.
+ */
+void swap(int *xp, int *ap)
+{
+	int tmp;
+
+	tmp  = *xp;
+	*xp = *ap;
+	*ap = tmp;
+}
+
+/**
+ * selection_sort - Sort an integer array using the Selection Sort algorithm.
+ * @array: Pointer to the integer array to be sorted.
+ * @size: The number of elements in the array.
+ *
+ * Return: void (No return value).
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		if (min_idx != i)
+			swap(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
+}
+
--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 int length(listint_t *head);
 void swap(int *xp, int *ap);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
