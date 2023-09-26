f140b0d8dd7868d94c038fb1290f65171c6a8a0a
Ssnnee <nandisamuelsne@gmail.com>
2023-09-25 17:34:56 -0700

    Trying to improve the selection sort algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,6 +25,7 @@ void swap(int *xp, int *ap)
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
+	int temp;
 
 	if (size < 2)
 		return;
@@ -32,13 +33,20 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
+		}
 
 		if (min_idx != i)
-			swap(&array[min_idx], &array[i]);
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+		}
+
 		print_array(array, size);
 	}
 }
-
